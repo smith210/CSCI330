e02449692a65005ac0a620b1be97af4c724ef5d0
ander664 <ander664@wwu.edu>
2019-04-26 17:40:28 -0700

    Added DeleteParser

---
+++ DeleteParser.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class DeleteParser {
+  private String relation;
+
+  DeleteParser(String input) {
+    Parser p = new Parser(input);
+    LinkedList<String> commands = p.parseCommandSet();
+    for(int i = 0; i < commands.size(); i++){
+      String currCommand = commands.get(i);
+
+    }
+  }
+  public String parseRelationName() {
+    return this.relation;
+  }
+}
