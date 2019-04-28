ebfcd8d9402b15f0ffaf850fa720b5eef4a8b3a4
ander664 <ander664@wwu.edu>
2019-04-27 23:11:10 -0700

    Uncommented final draft, paired programming

--- Main.java
+++ Main.java
@@ -4,8 +4,8 @@ CSCI 330 - Spring 2019
 File Name : Main.java
 */
 import java.io.*;
-public class Main{
 
+public class Main{
 	public static void main(String[] args) throws IOException {
 		LexicalAnalyzer surly = new LexicalAnalyzer();
 		surly.run(args[0]);
