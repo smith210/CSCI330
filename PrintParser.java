e08b8cd879c800a1bb62ef815c78338a777a0c50
ander664 <ander664@wwu.edu>
2019-04-27 22:17:20 -0700

    Uncommented final draft, paired programming

--- PrintParser.java
+++ PrintParser.java
@@ -1,5 +1,5 @@
 /*
-Name: Martin Smith
+Name: Martin Smith, Eric Anderson
 CSCI 330 - Spring 2019
 File Name : PrintParser.java
 */
@@ -9,17 +9,11 @@ public class PrintParser{
 	private String[] information;
 
 	PrintParser(String command){
-		information = new String[1];		
+		information = new String[1];
 		information[0] = "";
 		int pointer = 0;
-		int frontIndex = 0;
-		int endIndex = 0;
 		int attrVar = 0;
 		boolean nameSet = false;
-		boolean inQuote = false;
-		boolean isValidParentheses = false;
-		boolean isEqualAttach = false;
-		boolean hitBreak = false;
 
 		Parser p = new Parser(command);
 		LinkedList<String> commands = p.parseCommandSet();
@@ -27,7 +21,7 @@ public class PrintParser{
 			String currCommand = commands.get(i);
 
 			if(!currCommand.equals(",") && !currCommand.equals(";")){
-				String[] newInformation = new String[attrVar + 1];				
+				String[] newInformation = new String[attrVar + 1];
 				if(!information[0].isEmpty()){
 					for(int j = 0; j < attrVar; j++){
 						newInformation[j] = information[j];
@@ -40,7 +34,7 @@ public class PrintParser{
 		}
 
 	}
-	
+
 	public String[] parseRelationNames(){
 		return this.information;
 	}
