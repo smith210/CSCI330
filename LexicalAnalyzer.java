7762d6d3408e5deab8c7b452d05b39a0395a375f
ander664 <ander664@wwu.edu>
2019-04-27 22:16:52 -0700

    Uncommented final draft, paired programming

--- LexicalAnalyzer.java
+++ LexicalAnalyzer.java
@@ -1,5 +1,5 @@
 /*
-Name: Martin Smith
+Name: Martin Smith, Eric Anderson
 CSCI 330 - Spring 2019
 File Name : LexicalAnalyzer.java
 */
@@ -35,7 +35,6 @@ public class LexicalAnalyzer{
 			t.add(catalogRelation);
 			t.add(catalogAttributes);
 			catalog.insertTuple(t);
-			surly.replace(catalog, 0);
 	}
 
 	public void sendLine(String command, Scanner s, SurlyDatabase database){
@@ -70,66 +69,129 @@ public class LexicalAnalyzer{
 															command.indexOf(';') + 1);
 					String nextCommand = command.substring(command.indexOf(';') + 1);
 					switch(typeCommand){//send command to specified parser
+
+
 						case "RELATION":
 							RelationParser rp = new RelationParser(inputCommand);
 							if(rp.parseAttributeCount() != -1){
 								Relation r = rp.parseRelations();
-								database.add(r);
-								updateCatalog(database, database.getRelation("CATALOG"), rp.parseRelationName(), rp.parseAttributeCount());
+								Relation search = database.getRelation(r.parseRelationName());
+								if(search.parseRelationName().isEmpty()){
+									database.add(r);
+									updateCatalog(database, database.getRelation("CATALOG"), rp.parseRelationName(), rp.parseAttributeCount());
+								}else{
+									System.out.println("Relation " + r.parseRelationName() + " already exists within CATALOG.\n");
+								}
 							}else{
 								System.out.println("ERROR - invalid syntax inputted");
 								System.exit(0);
 							}
 							break;
+
+
 						case "INSERT":
 							InsertParser ip = new InsertParser(inputCommand);
 							if(ip.parseAttributeCount() != -1){
-
 								Relation r = database.getRelation(ip.parseRelationName());
-								int index = database.getRelationIndex(ip.parseRelationName());
-								Tuple tp = ip.parseTuple();
-								r.insertTuple(tp);
-								database.replace(r, index);
+								boolean canAddTuple = false;
+								  if (!r.parseRelationName().isEmpty()) {
+										if(ip.parseAttributeCount() <= r.parseRelationSchema().size()){
+											Tuple tp = ip.parseTuple();
+											for (int x = 0; x < tp.parseTupleValues().size(); x++) {
+												String type = r.parseRelationSchema().get(x).parseAttributeType();
+												int constrict = r.parseRelationSchema().get(x).parseAttributeLength();
+												if(constrict >= tp.parseTupleValues().get(x).parseAttName().length()){
+													canAddTuple = true;
+													if (type.equals("NUM")) {
+														for(int y = 0; y < tp.parseTupleValues().get(x).parseAttName().length(); y++) {
+															if (!Character.isDigit(tp.parseTupleValues().get(x).parseAttName().charAt(y))) {
+																canAddTuple = false;
+															}
+														}
+													}
+												}
+												else {
+													canAddTuple = false;
+												}
+
+											}
+											if(canAddTuple){
+												r.insertTuple(tp);
+												canAddTuple = false;
+											}
+											else{
+												System.out.println("Invalid length or invalid type being inserted.\n");
+											}
+										}
+										else{
+												System.out.print("Cannot insert " + ip.parseAttributeCount() + " attributes into " + ip.parseRelationName());
+												System.out.println(" (max = " + r.parseRelationSchema().size() + ").\n");
+										}
+									}
+									else {
+										System.out.println("Relation " + ip.parseRelationName() + " that you are trying to insert into does not exist within CATALOG.\n");
+									}
 
 							}else{
 								System.out.println("ERROR - invalid syntax inputted");
 								System.exit(0);
 							}
 							break;
+
+
 						case "PRINT"://evaluate PRINT command
 							PrintParser pp = new PrintParser(inputCommand);
-
 							String[] relationsToPrint = pp.parseRelationNames();
 							for(int i = 0; i < relationsToPrint.length; i++){
-
 								Relation r = database.getRelation(relationsToPrint[i]);
 								LinkedList<Tuple> tu = r.parseRelationTuples();
 								LinkedList<Attribute> sch = r.parseRelationSchema();
-								System.out.print(" ******************************\n");
-								System.out.println(" | " + r.parseRelationName() + "                     |");
-								System.out.print(" ------------------------------\n");
-								System.out.print(" |");
+								if(r.parseRelationName().isEmpty()){
+									  System.out.println("Relation " + pp.parseRelationNames()[i] + " doesn't exist within CATALOG.\n");
+										break;
+								}
+								int longestTuple = longestTuple(tu, sch);
+								int count = 0;
+								System.out.print(" ");
+								for (int x = 0; x < ((longestTuple+6)*sch.size()); x++) {
+									System.out.print("*");
+									count++;
+								}
+								System.out.println(" ");
+								System.out.print(" | " + r.parseRelationName());
+								for (int x = 0; x < count - r.parseRelationName().length() - 3; x++) {
+									System.out.print(" ");
+								}
+								System.out.print("|\n ");
+								for (int x = 0; x < count; x++) {
+									System.out.print("-");
+								}
+								System.out.print(" \n |");
 								for(int j = 0; j < sch.size(); j++){
 									int length1 = 0;
 									System.out.print(" " + sch.get(j).parseAttributeName() + " ");
-									while (length1 + sch.get(j).parseAttributeName().length() < 11) {
-										if (length1 + sch.get(j).parseAttributeName().length() == 10) {
+									while (length1 + sch.get(j).parseAttributeName().length() < (longestTuple + 2)) {
+										if (length1 + sch.get(j).parseAttributeName().length() == longestTuple+1) {
 											System.out.print(" |");
 										}
 											System.out.print(" ");
 											length1++;
 									}
 								}
-								System.out.println("|");
+								System.out.print("\n ");
+								for (int x = 0; x < count; x++) {
+									System.out.print("-");
+								}
+								System.out.println(" ");
 								for(int k = 0; k < tu.size(); k++){
 									System.out.print(" |");
 									LinkedList<AttributeValue> temp = tu.get(k).parseTupleValues();
 									for(int l = 0; l < temp.size(); l++){
 										int length2 = 0;
 										System.out.print(" " + temp.get(l).parseAttName());
-										while (length2 + temp.get(l).parseAttName().length() < 13) {
-											  if (length2 + temp.get(l).parseAttName().length() == 12) {
-													System.out.print("|");
+										while (length2 + temp.get(l).parseAttName().length() < (longestTuple + 1)) {
+											  if (length2 + temp.get(l).parseAttName().length() == longestTuple) {
+													System.out.print("   |");
 												}
 												System.out.print(" ");
 												length2++;
@@ -137,25 +199,37 @@ public class LexicalAnalyzer{
 									}
 									System.out.println(" ");
 								}
-								System.out.println(" ******************************\n");
+								System.out.print(" ");
+								for (int x = 0; x < count; x++) {
+									System.out.print("*");
+								}
+								System.out.println(" \n");
 							}
 
 							break;
+
+
 						case "DELETE":
 							DeleteParser dlt = new DeleteParser(inputCommand);
 							String relationDLT = dlt.parseRelationName();
 							Relation r = database.getRelation(relationDLT);
-							r.deleteTuples();
-
+							if(!relationDLT.equals("CATALOG") && !r.parseRelationName().isEmpty()){
+								r.deleteTuples();
+						 }
+						 else {
+						 	System.out.println("Cannot delete tuples from " + relationDLT + ".");
+						 }
 							break;
+
+
 						case "DESTROY":
 							DestroyParser dst = new DestroyParser(inputCommand);
 							String relationDST = dst.parseRelationName();
-							int index = database.getRelationIndex(relationDST);
-							database.replace(new Relation(), index);
 							database.destroyRelation(relationDST);
 							break;
-						default://command is not recognized
+
+
+						default: //command is not recognized
 
 					}
 					//remove excess space from nextCommand
@@ -174,6 +248,23 @@ public class LexicalAnalyzer{
 			}
 		}
 
+	}
+	public int longestTuple(LinkedList<Tuple> group1, LinkedList<Attribute> group2) {
+    int length = 1;
+		for(int k = 0; k < group1.size(); k++) {
+		  LinkedList<AttributeValue> neww1 = group1.get(k).parseTupleValues();
+			for(int i = 0; i < neww1.size(); i++) {
+				if (neww1.get(i).parseAttName().length() > length) {
+					length = neww1.get(i).parseAttName().length();
+				}
+		  }
+		}
+		for(int k = 0; k < group2.size(); k++) {
+		  if (group2.get(k).parseAttributeName().length() > length) {
+				length = group2.get(k).parseAttributeName().length();
+			}
+		}
+		return length;
 	}
 
 }
