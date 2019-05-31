77dcd0cfab8d632d6c5c1643856671038ad8eee2
ander664 <ander664@wwu.edu>
2019-05-30 22:48:36 -0700

    More where clause work

--- SURLY/Condition.java
+++ SURLY/Condition.java
@@ -1,7 +1,7 @@
 import java.util.*;
 
 public class Condition{
-	private String left; 
+	private String left;
 	private String conditionEval;
 	private String right;
 
@@ -14,7 +14,7 @@ public class Condition{
 	public void setLeft(String left){ this.left = left; }
 	public void setRight(String right){ this.right = right; }
 	public void setEvaluator(String conditionEval){ this.conditionEval = conditionEval; }
-	
+
 	public String returnLeft(){return left;}
 	public String returnRight(){return right;}
 	public String returnEvaluator(){return conditionEval; }
@@ -27,54 +27,66 @@ public class Condition{
 		}
 	}
 
-	public boolean typeValid(String type){		
-		if(!conditionEval.equals("=") && !conditionEval.equals("!=") && type.equals("CHAR"){
+	public boolean typeValid(String type){
+		if(!conditionEval.equals("=") && !conditionEval.equals("!=") && type.equals("CHAR")) {
 			return false;
 		}else{
 			return true;
 		}
 	}
 
-	public void parseTuples(int index, LinkedList<Tuple> tuples){
-	    LinkedList<Tuple> chosenOne = new LinkedList<Tuple>();
-		for(int i = 0; i < tuples.size(); i++){
-			Tuple curr = tuples.get(i);
+	public LinkedList<Tuple> parseTuples(int index, LinkedList<Tuple> allTuples, LinkedList<Tuple> tupleRef, String command){
+	  LinkedList<Tuple> chosenOnes = new LinkedList<Tuple>();
+		for(int i = 0; i < allTuples.size(); i++){
+			Tuple curr = allTuples.get(i);
 			AttributeValue evaluating = curr.parseTupleValues().get(index);
 			switch(conditionEval){
 				case "=":
-					if(right.equals(evaluating.parseAttName()){
-						chosenOne.add(curr);
+					if(right.equals(evaluating.parseAttName())) {
+						chosenOnes.add(curr);
+						tupleRef.remove(curr);
 					}
 					break;
 				case "!=":
-					if(!right.equals(evaluating.parseAttName()){
-						chosenOne.add(curr);
+					if(!right.equals(evaluating.parseAttName())){
+						chosenOnes.add(curr);
+						tupleRef.remove(curr);
 					}
 					break;
 				case "<":
 					if(Integer.parseInt(right) < Integer.parseInt(evaluating.parseAttName())){
-						chosenOne.add(curr);
+						chosenOnes.add(curr);
+						tupleRef.remove(curr);
 					}
 					break;
 				case ">":
 					if(Integer.parseInt(right) > Integer.parseInt(evaluating.parseAttName())){
-						chosenOne.add(curr);
+						chosenOnes.add(curr);
+						tupleRef.remove(curr);
 					}
 					break;
 				case "<=":
 					if(Integer.parseInt(right) <= Integer.parseInt(evaluating.parseAttName())){
-						chosenOne.add(curr);
+						chosenOnes.add(curr);
+						tupleRef.remove(curr);
 					}
 					break;
 				case ">=":
 					if(Integer.parseInt(right) >= Integer.parseInt(evaluating.parseAttName())){
-						chosenOne.add(curr);
+						chosenOnes.add(curr);
+						tupleRef.remove(curr);
 					}
 					break;
 				default://error
 
 			}
 
+		}
+		if(command.equals("DELETE")) {
+			return tupleRef;
+		}
+		else {
+			return chosenOnes;
 		}
 	}
 
