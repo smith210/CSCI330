175eb70c185e242a5a34e957e9d959943d19caed
ander664 <ander664@wwu.edu>
2019-05-30 22:48:51 -0700

    More where clause work

--- SURLY/ConditionList.java
+++ SURLY/ConditionList.java
@@ -13,24 +13,25 @@ public class ConditionList{
 
 	public LinkedList<Condition> retrieveList(){ return cList; }
 
-	public LinkedList<Tuple> evalAllConds(Relation r/*, String command*/){//evaluate each condition
+	public LinkedList<Tuple> evalAllConds(Relation r, String command){//evaluate each condition
+		LinkedList<Tuple> trueConds = new LinkedList<Tuple>();
 		LinkedList<Tuple> chosenOnes = new LinkedList<Tuple>();
+		LinkedList<Tuple> tupleRef = r.parseRelationTuples();
 		for(int i = 0; i < cList.size(); i++){
 			Condition curr = cList.get(i);
 			String left = curr.returnLeft();
-			if(r.inSchema(left) != -1 && curr.typeValid(r.parseRelationSchema().get(r.inSchema(left)).parseAttributeType())){//search
-				chosenOnes.add(curr.parseTuples(r.inSchema(left), r.parseRelationTuples()));
-				/*
-				if(command.equals("DELETE")){//add to delete list
-					--two linked lists, one to add one to not have
-				}else{ //add to keep list
-					--at end of for loop, determine command, if delete return not have, else reutn have
-				}	
-				*/
+			if(r.inSchema(left) != -1 && curr.typeValid(r.parseRelationSchema().get(r.inSchema(left)).parseAttributeType())) {//search
+				trueConds = curr.parseTuples(r.inSchema(left), r.parseRelationTuples(), tupleRef, command);
+				chosenOnes.addAll(trueConds);
 			}
-			
+
+		  // if(command.equals("DELETE")){ //add to delete list; --two linked lists, one to add one to not have
+			//   return tupleRef;
+			// }
+			// else if (command.equals("SELECT")){ //add to keep list; --at end of for loop, determine command, if delete return not have, else return have
+			//   return chosenOnes;
+			// }
 		}
 		return chosenOnes;
 	}
-
 }
