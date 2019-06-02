2987ec6edd750efb440e02db1d7f07b6a3c36436
ander664 <ander664@wwu.edu>
2019-06-01 18:27:14 -0700

    implemented PROJECT

--- LexicalAnalyzer.java
+++ LexicalAnalyzer.java
@@ -8,7 +8,15 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
-public class LexicalAnalyzer{ // the heart of our program
+public class LexicalAnalyzer{
+
+	private Parser parser;
+	private SurlyDatabase surly;
+
+	LexicalAnalyzer(){
+		surly = new SurlyDatabase();
+		parser = new Parser();
+	}
 
 	public void run(String filename){
 		File contain = null;
@@ -21,23 +29,170 @@ public class LexicalAnalyzer{ // the heart of our program
 			System.out.println("ERROR - no such file: " + filename);
 			System.exit(0);
 		}
-		SurlyDatabase surly = new SurlyDatabase();
-		sendLine("", line, surly);
-	}
-	public void updateCatalog(SurlyDatabase surly, Relation catalog, String name, int attr){
-			Tuple t = new Tuple();
-			AttributeValue catalogRelation = new AttributeValue();
-			AttributeValue catalogAttributes = new AttributeValue();
-			catalogRelation.setValue("CATALOG");
-			catalogAttributes.setValue("CATALOG");
-			catalogRelation.setName(name);
-			catalogAttributes.setName(Integer.toString(attr));
-			t.add(catalogRelation);
-			t.add(catalogAttributes);
-			catalog.insertTuple(t);
+		sendLine("", line);
 	}
 
-	public void sendLine(String command, Scanner s, SurlyDatabase database){
+	private void updateCatalog(Relation catalog, String name, int attr){
+		Tuple t = new Tuple();
+		AttributeValue catalogRelation = new AttributeValue();
+		AttributeValue catalogAttributes = new AttributeValue();
+		catalogRelation.setValue("CATALOG");
+		catalogAttributes.setValue("CATALOG");
+		catalogRelation.setName(name);
+		catalogAttributes.setName(Integer.toString(attr));
+		t.add(catalogRelation);
+		t.add(catalogAttributes);
+		catalog.insertTuple(t);
+	}
+
+	private void parseInstructions(String command){
+
+		switch(command){
+			case "RELATION"://Parse Relation commands
+				RelationParser rp = new RelationParser(parser);
+				if(rp.parseAttributeCount() != -1){
+					Relation r = rp.parseRelations();
+					Relation search = surly.getRelation(r.parseRelationName());
+					if(search.getTemp()){
+						System.out.println(search.parseRelationName() + "is temp");
+						surly.destroyRelation(search.parseRelationName());
+						search = surly.getRelation(r.parseRelationName());
+					}
+					if(search.parseRelationName().isEmpty()){
+						surly.add(r);
+						updateCatalog(surly.getRelation("CATALOG"), rp.parseRelationName(), rp.parseAttributeCount());
+					}else{
+						System.out.println("Relation " + r.parseRelationName() + " already exists within CATALOG.\n");
+					}
+
+				}else{
+					System.out.println("ERROR - invalid syntax inputted");//invalid syntax
+				}
+				break;
+			case "INSERT"://parse Insert commands
+				InsertParser ip = new InsertParser(parser);
+				if(ip.parseAttributeCount() != -1){
+					Relation r = surly.getRelation(ip.parseRelationName());
+					if(!r.parseRelationName().isEmpty()){//relation exists
+						if(ip.hasValidAttNum(r.parseRelationSchema().size())){//valid number of attributes inserting
+							/*for(int x = 0; x < r.parseRelationSchema().size(); x++){
+								ip.implementSize(x, r.parseRelationSchema().get(x).parseAttributeLength());
+							}*/
+							if(ip.isValid(r)){
+								r.insertTuple(ip.parseTuple());
+							}else{
+								System.out.println("Cannot INSERT into " + r.parseRelationName() + ": temporary relation.");
+							}
+						}
+					}
+				}
+				break;
+			case "PRINT"://evaluate PRINT command
+				PrintParser pp = new PrintParser(parser);
+				String[] relationsToPrint = pp.parseRelationNames();
+				for(int i = 0; i < relationsToPrint.length; i++){//for each Relation to print
+					Relation r = surly.getRelation(relationsToPrint[i]);
+					if(r.parseRelationName().isEmpty()){//don't print non-existant relations
+						  System.out.println("Relation " + pp.parseRelationNames()[i] + " doesn't exist within CATALOG.\n");
+					}else{
+						r.display();
+					}
+				}
+				break;
+			case "DELETE": // Deletes relations
+				DeleteParser dlt = new DeleteParser(parser);
+				LinkedList<String> relations = dlt.parseCommands();
+				String relationDLT = dlt.parseRelationName();
+				Relation r = surly.getRelation(relationDLT);
+
+				LinkedList<Tuple> empty = new LinkedList<Tuple>();
+				//LinkedList<ConditionList
+				//ConditionList conditionList = new ConditionList();
+
+				if(!r.parseRelationName().isEmpty() && !r.getTemp()){//make sure not empty
+					if(!parser.hasWhere()){
+						r.deleteTuples(empty);
+					}
+					else {//specialized delete statement
+						//LinkedList<Condition> setConditions = conditionList.retrieveList();
+						//r.deleteTuples(conditionList.evalAllConds(r, command));
+						AllConditions conditions = dlt.getConditions();
+						System.out.println(conditions.size());
+						//for(int i = 0; i < conditions.size(); i++){
+
+
+							r.deleteTuples(conditions.evaluateConditions(r));
+
+						//}
+					}
+			 	}else{
+					System.out.println("Cannot DELETE from " + relationDLT + ": temporary relation.");
+				}
+				break;
+			case "DESTROY": // Destroys single relation one at a time
+				DestroyParser dst = new DestroyParser(parser);
+				String relationDST = dst.parseRelationName();
+				Relation rel = surly.getRelation(dst.parseRelationName());
+				if(!rel.getTemp()){
+					surly.destroyRelation(relationDST);
+				}else{
+					System.out.println("Cannot DESTROY " + relationDST + ": temporary relation.");
+				}
+				break;
+			default: // Deal with temporary relations
+				if(parser.hasEqual()){ //valid temporary relation
+					Relation currTemp = surly.getRelation(command);
+					if(currTemp.parseRelationName().length() != 0){
+						if(currTemp.getTemp()){
+							surly.destroyRelation(currTemp.parseRelationName());
+						}else{ //relation is not temp, DO NOT OVERWRITE!
+							System.out.println("Cannot overwrite base relation!");
+							break;
+						}
+					}
+					switch(parser.getSecondaryName()){
+						case "SELECT":
+							SelectParser sPsr = new SelectParser(parser);
+							sPsr.addRelation(surly);
+							Relation temp1 = sPsr.getTempRelation();
+							temp1.tempBuff();
+							if(temp1.parseRelationName().length() != 0){
+								surly.add(temp1);
+								updateCatalog(surly.getRelation("CATALOG"), temp1.parseRelationName(), temp1.parseRelationSchema().size());
+							}
+							break;
+						case "PROJECT":
+							ProjectParser prj = new ProjectParser(parser);
+							if(prj.isValid()){
+								prj.addInfo(surly);
+								prj.addRelation(surly);
+								Relation temp2 = prj.getTempRelation();
+								temp2.tempBuff();
+								System.out.println("HERE " + temp2.parseRelationName());
+								if(temp2.parseRelationName().length() != 0){
+									surly.add(temp2);
+									updateCatalog(surly.getRelation("CATALOG"), temp2.parseRelationName(), temp2.parseRelationSchema().size());
+								}
+							}
+							break;
+						case "JOIN":
+							JoinParser jPsr = new JoinParser(parser);
+							if(jPsr.isValid()){
+
+
+							}
+							break;
+						default://not valid temporary syntax
+
+					}
+
+				}
+
+		}
+
+	}
+
+	private void sendLine(String command, Scanner s){
 		String currLine = "";
 		if(s.hasNextLine()){
 			currLine = s.nextLine();
@@ -47,7 +202,7 @@ public class LexicalAnalyzer{ // the heart of our program
 
 
 		if(currLine.isEmpty()){//don't parse empty lines
-			sendLine(command, s, database);
+			sendLine(command, s);
 		}else{
 			int pointer = 0;
 			while(pointer != currLine.length() && Character.isWhitespace(currLine.charAt(pointer))){
@@ -55,183 +210,26 @@ public class LexicalAnalyzer{ // the heart of our program
 			}
 			if(pointer != 0){
 				if(pointer == currLine.length()){
-					sendLine(command,s, database);
+					sendLine(command,s);
 				}else{
 					currLine = currLine.substring(pointer);
 				}
 			}
 
-			if(currLine.charAt(0) != '#'){//do not parse comments
+			if(!currLine.contains("#")){//do not parse comments
 				command = command.concat(currLine);
 				if(command.contains(";")){//end of command has been reached
-					String typeCommand = command.substring(0, command.indexOf(' '));
-					String inputCommand = command.substring(command.indexOf(' ') + 1, command.indexOf(';') + 1);
+					parser = new Parser(command);
+					//parser.printContent();
 					String nextCommand = command.substring(command.indexOf(';') + 1);
+					System.out.println(command);
+					if(!parser.hasCatalog()){
+						parseInstructions(parser.getRelationName());
 
-
-					switch(typeCommand){//send command to specified parser
-
-						case "RELATION":
-							RelationParser rp = new RelationParser(inputCommand);
-							if(rp.parseAttributeCount() != -1){
-								Relation r = rp.parseRelations();
-								Relation search = database.getRelation(r.parseRelationName());
-								if(search.parseRelationName().isEmpty()){
-									database.add(r);
-									updateCatalog(database, database.getRelation("CATALOG"), rp.parseRelationName(), rp.parseAttributeCount());
-								}
-								else{
-									System.out.println("Relation " + r.parseRelationName() + " already exists within CATALOG.\n");
-								}
-							}else{
-								System.out.println("ERROR - invalid syntax.");
-								System.exit(0);
-							}
-							break;
-
-
-						case "INSERT": // checks several potential errors below
-							InsertParser ip = new InsertParser(inputCommand);
-							if(ip.parseAttributeCount() != -1){
-								Relation r = database.getRelation(ip.parseRelationName());
-								boolean canAddTuple = false;
-								  if (!r.parseRelationName().isEmpty()) {
-										if(ip.parseAttributeCount() <= r.parseRelationSchema().size()){
-											Tuple tp = ip.parseTuple();
-											for (int x = 0; x < tp.parseTupleValues().size(); x++) {
-												String type = r.parseRelationSchema().get(x).parseAttributeType();
-												int trueLength = r.parseRelationSchema().get(x).parseAttributeLength();
-												if(trueLength >= tp.parseTupleValues().get(x).parseAttName().length()){
-													canAddTuple = true;
-													if (type.equals("NUM")) {
-														for(int y = 0; y < tp.parseTupleValues().get(x).parseAttName().length(); y++) {
-															if (!Character.isDigit(tp.parseTupleValues().get(x).parseAttName().charAt(y))) {
-																canAddTuple = false;
-															}
-														}
-													}
-												}
-												else {
-													canAddTuple = false;
-												}
-
-											}
-											if(canAddTuple){
-												r.insertTuple(tp);
-												canAddTuple = false;
-											}
-											else{
-												System.out.println("Invalid length or invalid type being inserted.\n");
-											}
-										}
-										else{
-												System.out.print("Cannot insert " + ip.parseAttributeCount() + " attributes into " + ip.parseRelationName());
-												System.out.println(" (max = " + r.parseRelationSchema().size() + ").\n");
-										}
-									}
-									else {
-										System.out.println("Relation " + ip.parseRelationName() + " that you are trying to insert into does not exist within CATALOG.\n");
-									}
-
-							}else{
-								System.out.println("ERROR - invalid syntax.");
-								System.exit(0);
-							}
-							break;
-
-
-						case "PRINT": // prints in format as assignment indicates
-							PrintParser pp = new PrintParser(inputCommand);
-							String[] relationsToPrint = pp.parseRelationNames();
-							for(int i = 0; i < relationsToPrint.length; i++){
-								Relation r = database.getRelation(relationsToPrint[i]);
-								LinkedList<Tuple> tu = r.parseRelationTuples();
-								LinkedList<Attribute> sch = r.parseRelationSchema();
-								if(r.parseRelationName().isEmpty()){
-									  System.out.println("Relation " + pp.parseRelationNames()[i] + " doesn't exist within CATALOG.\n");
-										break;
-								}
-								int count = 0;
-								int longestTuple = longestTuple(tu, sch); // see bottom of file
-								System.out.print(" ");
-								for (int x = 0; x < ((longestTuple+6)*sch.size()); x++) {
-									System.out.print("*");
-									count++; // establishes length of asterisks and dashes, depending on length of longest tuple
-								}
-								System.out.println(" ");
-								System.out.print(" | " + r.parseRelationName());
-								for (int x = 0; x < count - r.parseRelationName().length() - 3; x++) {
-									System.out.print(" ");
-								}
-								System.out.print("|\n ");
-								for (int x = 0; x < count; x++) {
-									System.out.print("-");
-								}
-								System.out.print(" \n |");
-								for(int j = 0; j < sch.size(); j++){
-									int length1 = 0;
-									System.out.print(" " + sch.get(j).parseAttributeName() + " ");
-									while (length1 + sch.get(j).parseAttributeName().length() < (longestTuple + 2)) {
-										if (length1 + sch.get(j).parseAttributeName().length() == longestTuple + 1) {
-											System.out.print(" |");
-										}
-											System.out.print(" ");
-											length1++;
-									}
-								}
-								System.out.print("\n ");
-								for (int x = 0; x < count; x++) {
-									System.out.print("-");
-								}
-								System.out.println(" ");
-								for(int k = 0; k < tu.size(); k++){
-									System.out.print(" |");
-									LinkedList<AttributeValue> temp = tu.get(k).parseTupleValues();
-									for(int l = 0; l < temp.size(); l++){
-										int length2 = 0;
-										System.out.print(" " + temp.get(l).parseAttName());
-										while (length2 + temp.get(l).parseAttName().length() < (longestTuple + 1)) {
-											  if (length2 + temp.get(l).parseAttName().length() == longestTuple) {
-													System.out.print("   |");
-												}
-												System.out.print(" ");
-												length2++;
-										}
-									}
-									System.out.println(" ");
-								}
-								System.out.print(" ");
-								for (int x = 0; x < count; x++) {
-									System.out.print("*");
-								}
-								System.out.println(" \n");
-							}
-
-							break;
-
-
-						case "DELETE": // Deletes single relation one at a time
-							DeleteParser dlt = new DeleteParser(inputCommand);
-							String relationDLT = dlt.parseRelationName();
-							Relation r = database.getRelation(relationDLT);
-							if(!relationDLT.equals("CATALOG") && !r.parseRelationName().isEmpty()){
-								r.deleteTuples();
-						 }
-						 else {
-						 	System.out.println("Cannot delete tuples from " + relationDLT + ".");
-						 }
-							break;
-
-
-						case "DESTROY": // Destroys single relation one at a time
-							DestroyParser dst = new DestroyParser(inputCommand);
-							String relationDST = dst.parseRelationName();
-							database.destroyRelation(relationDST);
-							break;
-
-
-						default: //command is not recognized
-
+					}else{
+						if(parser.getRelationName().equals("PRINT")){
+							parseInstructions(parser.getRelationName());
+						}
 					}
 					//remove excess space from nextCommand
 					int linePointer = 0;
@@ -240,34 +238,15 @@ public class LexicalAnalyzer{ // the heart of our program
 						linePointer++;
 					}
 					nextCommand = nextCommand.substring(linePointer);
-					sendLine(nextCommand, s, database);//start with new command
+					sendLine(nextCommand, s);//start with new command
 				}else{
-					sendLine(command,s, database);//keep concatenating command until ';' seen
+					sendLine(command,s);//keep concatenating command until ';' seen
 				}
 			}else{
-				sendLine(command, s, database);//not parsing comments
+				sendLine("", s);//not parsing comments
 			}
 		}
 
 	}
 
-	public int longestTuple(LinkedList<Tuple> group1, LinkedList<Attribute> group2) {
-    int length = 1;
-		for(int k = 0; k < group1.size(); k++) {
-		  LinkedList<AttributeValue> neww1 = group1.get(k).parseTupleValues();
-			for(int i = 0; i < neww1.size(); i++) {
-				if (neww1.get(i).parseAttName().length() > length) {
-					length = neww1.get(i).parseAttName().length();
-				}
-		  }
-		}
-		for(int k = 0; k < group2.size(); k++) {
-		  if (group2.get(k).parseAttributeName().length() > length) {
-				length = group2.get(k).parseAttributeName().length();
-			}
-		}
-		return length;
-	}
 }
-
-// END
